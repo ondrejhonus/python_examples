14f7d39be6882d2484c30f6e80326639bce13b51
ondrejhonus <honusondrej@gmail.com>
2024-09-24 16:05:22 +0200

    russian roulette point system

--- russian_rulette.py
+++ russian_rulette.py
@@ -3,20 +3,28 @@ import time
 
 input("Press enter to spin the chamber")
 chambered = random.randrange(1,6)
-for i in range(chambered * 2):
-    input("It's your turn, press enter to shoot")
-    if(i == chambered):
-        print("You lost!")
-        break
-    else:
-        print("You didn't get shot")
-    i+=1
-    if(i+1 == chambered):
-        print("It's the computers turn.")
-        time.sleep(3)
-        print("The computer lost")
-        break
-    else:
-        print("It's the computers turn.")
-        time.sleep(3)
-        print("The computer didn't get shot")
+p_points = 0
+c_points = 0
+play_more = 'y'
+while play_more == 'y' or play_more == '':
+    for i in range(chambered * 2):
+        input("It's your turn, press enter to shoot")
+        if(i == chambered):
+            print("You lost!")
+            c_points+=1
+            break
+        else:
+            print("You didn't get shot")
+        i+=1
+        if(i+1 == chambered):
+            print("It's the computers turn.")
+            time.sleep(3)
+            print("The computer lost")
+            p_points+=1
+            break
+        else:
+            print("It's the computers turn.")
+            time.sleep(3)
+            print("The computer didn't get shot")
+    print(f"Computer: {c_points} You: {p_points}")
+    play_more = input("Do you want to play again?")
